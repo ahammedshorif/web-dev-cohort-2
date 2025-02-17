b1c42f97243a0c899d0db40613fbdff119a9f98a
ahammedshorf <ahammedshorif20@gmail.com>
2025-02-17 22:15:17 +0600

    week1 c3

--- week1/object.js
+++ week1/object.js
@@ -14,5 +14,36 @@ function printObj(animal){
     console.log("animal " + animal["name"] + " has " + animal["letCount"] );
 }
 
-printObj(dog)
-printObj(cat)
+// printObj(dog)
+// printObj(cat)
+
+// convert object to string
+const catSting = JSON.stringify(cat);
+
+// convert string to object
+const catObj = JSON.parse(catSting);
+
+
+// console.log(catSting);
+// console.log(catObj);
+
+
+
+
+const sampleObject = {
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3',
+};
+
+let key = Object.keys(sampleObject);
+let value = Object.values(sampleObject);
+let entries = Object.entries(sampleObject);
+let assings = Object.assign(sampleObject, {name: "shorif", age: "24"});
+
+console.log(key);
+console.log(value);
+console.log(entries);
+console.log(assings);
+
+
