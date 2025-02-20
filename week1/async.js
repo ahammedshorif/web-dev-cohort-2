d0212f53c012f5bcbe6bd2b6c01ae8e44d1cc368
ahammedshorf <ahammedshorif20@gmail.com>
2025-02-20 23:53:43 +0600

    week1 c6

--- week1/async.js
+++ week1/async.js
@@ -15,3 +15,53 @@ function findSumOfNums(){
 
 setTimeout(findSumOfNums,2000);
 console.log("Hello");
+
+
+
+const { log } = require("console");
+//some async function
+
+// fs.readFile()
+// fetch()
+// timeout()
+// timeInterval()
+
+
+
+const fs = require("fs");
+
+// function readTheFile(cb){
+//     fs.writeFileSync("a.txt",  "hello world");
+// }
+// readTheFile()
+
+// fs.appendFileSync("a.txt"," new world");
+
+
+
+// promise
+
+  //examples
+
+function readTheFile(){
+    console.log("inside readTheFile");
+    
+    return new Promise(function(resolve, reject){
+        console.log("inside Promise");
+        
+        fs.readFile("a.txt","utf-8", function(err,data){
+            console.log("before resolve promise");
+            
+            resolve(data);
+            console.log("after resolve promise");
+            
+        })
+    })
+}
+
+function onDone(data){
+    console.log(data);
+}
+
+let a= readTheFile();
+a.then(onDone);
