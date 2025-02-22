081a4f1f084bac9a3e433742dc937e808f2f09a3
ahammedshorf <ahammedshorif20@gmail.com>
2025-02-22 23:26:04 +0600

    week1 c2

--- week2/recapWeek1.js
+++ week2/recapWeek1.js
@@ -21,4 +21,17 @@ function cube(n){
 
  // async function
  // setTimeout, setTimeInterval, readFile, writeFile etc.
+
+ function loop100(){
+    let sum=0;
+    for(let i=0; i<100; i++){
+        sum+=i;
+    }
+    console.log(sum);
+    
+ }
+
+ setTimeout(loop100,5000);
+
+ console.log("Here we go...");
  
