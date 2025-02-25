1871e616f4a8bfc1567453b7babf6718e59f0d90
ahammedshorf <ahammedshorif20@gmail.com>
2025-02-26 03:10:19 +0600

    week2 c4

--- week2/recapWeek1.js
+++ week2/recapWeek1.js
@@ -1,39 +1,69 @@
-//callback
+// //callback
 
-function sumOfPower(a,b, callback){
-    let x = callback(a);
-    let y = callback(b);
-    return x + y;
-}
+// function sumOfPower(a,b, callback){
+//     let x = callback(a);
+//     let y = callback(b);
+//     return x + y;
+// }
 
-function square(n){
-   return  n*n;
-}
+// function square(n){
+//    return  n*n;
+// }
 
-function cube(n){
-    return n*n*n;
-}
-
-
- console.log(sumOfPower(2,3,square));
- console.log(sumOfPower(2,3,cube));
+// function cube(n){
+//     return n*n*n;
+// }
 
 
- // async function
- // setTimeout, setTimeInterval, readFile, writeFile etc.
+//  console.log(sumOfPower(2,3,square));
+//  console.log(sumOfPower(2,3,cube));
 
- function loop100(){
-    let sum=0;
-    for(let i=0; i<100; i++){
-        sum+=i;
-    }
-    console.log(sum);
+
+//  // async function
+//  // setTimeout, setTimeInterval, readFile, writeFile etc.
+
+//  function loop100(){
+//     let sum=0;
+//     for(let i=0; i<100; i++){
+//         sum+=i;
+//     }
+//     console.log(sum);
     
- }
+//  }
 
- setTimeout(loop100,5000);
+//  setTimeout(loop100,5000);
 
- console.log("Here we go...");
+//  console.log("Here we go...");
+
+
+// *****
+ console.log("at the top 1");
+
+ function PormisFunction(){
+    console.log("inside function 3");
+    
+    return new Promise((reslove)=>{
+        console.log("inside Promise 4");
+        
+        setTimeout(()=>{
+            console.log("setTimeout called 5");
+
+            reslove("resolve done");
+            
+        })
+    })
+ };
+ 
+ console.log("In the middle 2");
+
+ PormisFunction().then((value)=>{
+    console.log("Promise resolved 6 ");
+    
+    console.log(value);
+    
+ })
+ 
+ 
 
 
 
