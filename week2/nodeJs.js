5991a851d1fabe230d2db11e22964f10ee1baa2d
ahammedshorf <ahammedshorif20@gmail.com>
2025-02-27 01:59:23 +0600

    week2 c6

---
+++ week2/nodeJs.js
@@ -0,0 +1,10 @@
+//http - hyper text transfer protocol
+
+// protocol: A protocal that is define for machines to communicate with forntend is talk to backend server
+
+
+
+ // HTTP (client side) - orotocol(http,https), adderss(ip,url,port ),Route, (header,body,query parameters) ,methods( post, get, put)
+
+ // HTTP (server side)- Response Headers, Response Body, Status code
+
