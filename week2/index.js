f848c8400442cbe7e52764f3f35393ecbdb6db74
ahammedshorf <ahammedshorif20@gmail.com>
2025-03-05 01:23:20 +0600

    week2 c10

--- week2/index.js
+++ week2/index.js
@@ -9,3 +9,25 @@ var users= [{
     address: ""
   }
 }];
+
+
+
+// map
+
+let arr = [4, 5, 6, 7];
+
+
+let newArr = arr.map((item)=>{
+  return item*2;
+})
+
+console.log(newArr);
+
+
+// filter
+
+let filterArr = arr.filter((item)=>{
+  return item%2==0;
+});
+
+console.log(filterArr);
