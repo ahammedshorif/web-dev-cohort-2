fdc9d30a0d563d4be76baab462f30a3488979c1e
ahammedshorf <ahammedshorif20@gmail.com>
2025-03-19 03:14:17 +0600

    modify testMongo.js get data from mongoD

--- week3/week3.2/mongoDb_server/testMongo.js
+++ week3/week3.2/mongoDb_server/testMongo.js
@@ -1,34 +1,57 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const app = express();
 
-const app= express()
+app.use(express.json());
 
+mongoose.connect("mongodb://localhost:27017/test");
 
-app.use(express.json())
-
-mongoose.connect('mongodb://localhost:27017/test');
-
-
-const Users = mongoose.model("Users",{
-    name: String,
-    username: String,
-    password: String,
+const Users = mongoose.model("Users", {
+  name: String,
+  username: String,
+  password: String,
 });
 
 
-const user = new Users({
-    name:"Ahammed shorif",
-    username: "ahmmedsh0rif",
-    password: "1234"
-})
+//** store data in mongodb database
 
-user.save();
-
-console.log("data seve")
-
-
-app.listen(3000,()=>{
-    console.log("listening on port 3000");
+function storeData(){
+    const user = new Users({
+        name:"shorif100",
+        username: "ahmmedsh0rif",
+        password: "1234"
+    })
     
-   })
+    user.save();
+    console.log("data seve")
+}
+
+//  storeData()
+
+
+
+//***get data**
+async function getData() {
+  try {
+    const docs = await Users.find({});
+    return docs;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+app.get("/", (req, res) => {
+  async function call() {
+    const result = await getData();
+    console.log(result);
+
+    res.json(result);
+  }
+  call()
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
