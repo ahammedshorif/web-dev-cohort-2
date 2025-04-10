58c7627bceb99a65c7d41a1bd18f75e2ed94231c
ahammedshorf <ahammedshorif20@gmail.com>
2025-04-10 23:35:11 +0600

    todo: backend input zod validation done

--- week5/project-5.2/backend/index.js
+++ week5/project-5.2/backend/index.js
@@ -1,9 +1,20 @@
-const express = require("express")
+const express = require("express");
+const { createTode, updateTodo } = require("./types");
 const app = express()
 
 app.use(express.json())
 
 app.post("/todo", (req,res)=>{
+    const createPayload = req.body;
+    const parsedPayload = createTode.safeParse(createPayload)
+
+    if(!parsedPayload.success){
+        res.status(403).json({
+            mag: "You send wrong inputs"
+        })
+        return;
+    }
+    //put it mongoDB
 
 })
 app.get("/todos",  (req,res)=>{
@@ -11,7 +22,15 @@ app.get("/todos",  (req,res)=>{
 })
 
 app.put("/completed", (req,res)=>{
-    
+    const updatePayload = req.body;
+    const parsedPayload= updateTodo.safeParse(updatePayload)
+
+    if(!parsedPayload.success){
+        res.status(403).json({
+            mag: "You send wrong inputs"
+        })
+        return;
+    }
 })
 
 app.listen(3000,()=>{
