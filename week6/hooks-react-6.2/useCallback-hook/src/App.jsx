730d85d758043b01628795b120866a48f0ef012f
ahammedshorf <ahammedshorif20@gmail.com>
2025-04-22 21:51:48 +0600

    explore useCallback & memo function

--- week6/hooks-react-6.2/useCallback-hook/src/App.jsx
+++ week6/hooks-react-6.2/useCallback-hook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,memo, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,11 +6,36 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  //prevent the rerender
+  const inputFunction= useCallback(()=>{
+    console.log("hello");
+    
+  },[])
+
   return (
     <>
-      <h1>useCallback</h1>
+      <BottonComponents inputFunction={inputFunction}></BottonComponents><br></br>
+      <button onClick={()=>{setCount(count+1)}}>click me {count}</button>
     </>
   )
+
+  
 }
+// every time parant rereder it will rerender
+// const BottonComponents = ()=>{
+//   console.log("child rerender");
+  
+//   return <div>
+//     <button>button click</button>
+//   </div>
+
+// it rerender only input change
+const BottonComponents = memo(({inputFunction})=>{
+  console.log("child rerender");
+  
+  return <div>
+    <button>button click</button>
+  </div>
+})
 
 export default App
