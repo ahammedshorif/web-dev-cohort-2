c48e1716ac7be91e527d8dd8912e6527049c160e
ahammedshorf <ahammedshorif20@gmail.com>
2025-04-26 23:22:25 +0600

    page navigate with button click Rigth & wong way

--- week7/react-routing-7.1/src/App.jsx
+++ week7/react-routing-7.1/src/App.jsx
@@ -1,21 +1,56 @@
 
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {BrowserRouter,Routes,Route, useNavigate} from "react-router-dom"
 import './App.css'
 import Home from "./components/Home"
 import About from "./components/About"
 
 function App() {
-
   return (
+    <div>
+      <p>This is not Right way , its page reload</p>
+      <div style={{display: "flex"}}>
+          <button onClick={()=>{
+            window.location.href= "/"
+          }}>Landing</button>
+
+          <button onClick={()=>{
+            window.location.href= "/home"
+          }}>Home</button>
+
+          <button onClick={()=>{
+            window.location.href= "/about"
+          }}>About</button>
+
+      </div>
       <BrowserRouter>
+           <Topbar></Topbar>
           <Routes>
              <Route path='/' element={<div>This is / page</div>} />
              <Route path='/home' element={<Home></Home>} />
              <Route path='/about' element={<About></About>} />
           </Routes>
       </BrowserRouter>
+    </div>
     
   )
+}
+
+function Topbar(){
+  const navigate= useNavigate()
+  return <div>
+      <p>this is the rigth way</p>
+        <button onClick={()=>{
+            navigate("/")
+          }}>Landing</button>
+
+          <button onClick={()=>{
+            navigate("/home")
+          }}>Home</button>
+
+          <button onClick={()=>{
+            navigate("/about")
+          }}>About</button>
+  </div>
 }
 
 export default App
