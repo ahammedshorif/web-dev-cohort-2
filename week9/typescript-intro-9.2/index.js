3755a6c1141aa32abed4bb9201345059fd8d8f3c
ahammedshorf <ahammedshorif20@gmail.com>
2025-05-19 02:12:58 +0600

    assign function type

--- week9/typescript-intro-9.2/index.js
+++ week9/typescript-intro-9.2/index.js
@@ -5,3 +5,16 @@ function sum(x, y) {
 }
 var result = sum(1, 2);
 console.log(result);
+function isLegal(a) {
+    if (a > 18)
+        return true;
+    else
+        return false;
+}
+// create a function that takes another function as input, and runs it after 1 second
+function runAfter1S(fn) {
+    setTimeout(fn, 1000);
+}
+runAfter1S(function () {
+    console.log("hello");
+});
--- week9/typescript-intro-9.2/index.ts
+++ week9/typescript-intro-9.2/index.ts
@@ -3,10 +3,27 @@ let a:string = "shorif"
 console.log(a)
 
 
-function sum(x:number,y:number){
+function sum(x:number,y:number):number{
     return x+y;
 }
 
-let result = sum(1,2)
+let result:number = sum(1,2)
 
 console.log(result)
+
+function isLegal(a:number):boolean{
+    if(a>18)
+        return true;
+    else
+        return false
+}
+
+// create a function that takes another function as input, and runs it after 1 second
+
+function runAfter1S(fn: ()=> void){
+    setTimeout(fn,1000)
+}
+
+runAfter1S(function(){
+    console.log("hello");
+})
