5d9a15b1f277c843f5faeac787bac950d085bcb0
ahammedshorf <ahammedshorif20@gmail.com>
2025-05-18 22:56:55 +0600

    typescript intro

---
+++ week9/typescript-intro-9.2/index.js
@@ -0,0 +1,7 @@
+var a = "shorif";
+console.log(a);
+function sum(x, y) {
+    return x + y;
+}
+var result = sum(1, 2);
+console.log(result);
---
+++ week9/typescript-intro-9.2/index.ts
@@ -0,0 +1,12 @@
+let a:string = "shorif"
+
+console.log(a)
+
+
+function sum(x:number,y:number){
+    return x+y;
+}
+
+let result = sum(1,2)
+
+console.log(result)
