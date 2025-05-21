ad6a9b56eb5beaf959c3b67e63b03a77e872fd2e
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-05-21 23:33:47 +0600

    first typescript react app

---
+++ week9/typescript-intro-9.2/react-typescript/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Todo title="todo 1" description="this is my first todo"></Todo>
+    </>
+  )
+}
+
+interface todoIntefaces{
+  title:string;
+  description:string
+  done?: boolean;
+}
+
+function Todo(props:todoIntefaces){
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+     
+  </div>
+}
+
+export default App
