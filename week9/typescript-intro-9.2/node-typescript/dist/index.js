ee12fa024af17eae239e765de318b2776450b31a
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-05-30 22:04:41 +0600

    typescript:explore intersection & Array

--- week9/typescript-intro-9.2/node-typescript/dist/index.js
+++ week9/typescript-intro-9.2/node-typescript/dist/index.js
@@ -20,3 +20,28 @@ class Employee {
 }
 const employee1 = new Employee("shorif", 24);
 console.log(employee1.name);
+function greet(variable) {
+    console.log(variable);
+}
+greet(10);
+greet("hello");
+const t = {
+    name: "shorif",
+    startDate: new Date(),
+    department: "HR"
+};
+function maxValue(arr) {
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+let mxv = maxValue([8, 5, 9, 3, 15]);
+console.log(mxv);
+// array of object
+function filterArray(arr) {
+    //>>>>
+}
--- week9/typescript-intro-9.2/node-typescript/index.ts
+++ week9/typescript-intro-9.2/node-typescript/index.ts
@@ -81,3 +81,70 @@ type Users = {
     lastname:string;
     age:number;
 }
+
+
+//or
+
+type costomType = number | string;
+
+function greet(variable:costomType){
+    console.log(variable)
+}
+
+greet(10);
+greet("hello");
+
+
+// intersection
+
+type Employees ={
+    name:string;
+    startDate:Date;
+}
+type Manager={
+    name:string;
+    department:string;
+}
+
+type TeamLead = Employees & Manager;
+
+const t: TeamLead ={
+    name:"shorif",
+    startDate:new Date(),
+    department: "HR"
+}
+
+
+//IQ: What is the difference between interface an type?
+
+
+// Array is typescript
+
+
+type arrType= number[];    //number[]
+
+function maxValue(arr: arrType){
+    let max=arr[0];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>max){
+            max= arr[i]
+        }
+    }
+  return max;
+}
+
+let mxv =maxValue([8,5,9,3,15]);
+console.log(mxv);
+
+
+type P = {
+    fistname:string;
+    lastname:string;
+    age:number;
+}
+
+// array of object
+function filterArray(arr: P[]){
+  //>>>>
+  
+}
