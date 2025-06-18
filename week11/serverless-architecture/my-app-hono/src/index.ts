ffea9140ac8da9058ecca71e27fbe06e57beab44
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-06-18 23:40:23 +0600

    get,post request wihe hono

--- week11/serverless-architecture/my-app-hono/src/index.ts
+++ week11/serverless-architecture/my-app-hono/src/index.ts
@@ -5,5 +5,15 @@ const app = new Hono()
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+app.post('/user', (c) => {
+
+  console.log(c.req.header('auth'));
+  console.log(c.req.header('prams'));
+  
+  
+  return c.json({
+     mes: 'hello user'
+  })
+})
 
 export default app
