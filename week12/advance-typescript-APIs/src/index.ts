dfa7841e2d02ac4b1bbe02b2bbc90ec14e595404
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-06-22 20:33:00 +0600

    recap typescript

--- week12/advance-typescript-APIs/src/index.ts
+++ week12/advance-typescript-APIs/src/index.ts
@@ -1,4 +1,21 @@
 interface User{
     name: string;
     age: number;
+};
+
+function sumOfAge(user1: User, user2: User){
+    return user1.age + user2.age;
 }
+
+let result = sumOfAge(
+    {
+        name:"shorif",
+        age: 24,
+    },
+    {
+        name:"shakib",
+        age: 26,
+    }
+)
+
+console.log(result);
