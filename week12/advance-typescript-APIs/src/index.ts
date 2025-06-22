1658ef4e131468472e2557f18f8674151e364a07
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-06-22 23:33:49 +0600

    typescript: pick

--- week12/advance-typescript-APIs/src/index.ts
+++ week12/advance-typescript-APIs/src/index.ts
@@ -1,21 +1,39 @@
+// interface User{
+//     name: string;
+//     age: number;
+// };
+
+// function sumOfAge(user1: User, user2: User){
+//     return user1.age + user2.age;
+// }
+
+// let result = sumOfAge(
+//     {
+//         name:"shorif",
+//         age: 24,
+//     },
+//     {
+//         name:"shakib",
+//         age: 26,
+//     }
+// )
+
+// console.log(result);
+
+
+
+// **pick**
 interface User{
+    id: string;
     name: string;
     age: number;
+    email: string;
+    password: string;
 };
 
-function sumOfAge(user1: User, user2: User){
-    return user1.age + user2.age;
+type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
+
+
+function updateUser(updateProps: UpdateProps){
+    //hit the database and update the user
 }
-
-let result = sumOfAge(
-    {
-        name:"shorif",
-        age: 24,
-    },
-    {
-        name:"shakib",
-        age: 26,
-    }
-)
-
-console.log(result);
