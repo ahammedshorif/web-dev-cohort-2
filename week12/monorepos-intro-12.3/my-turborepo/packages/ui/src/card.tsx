c292dd6a7a8f267da1235b5156c66eacd2dea074
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-06-26 23:10:26 +0600

    web page change ui

--- week12/monorepos-intro-12.3/my-turborepo/packages/ui/src/card.tsx
+++ week12/monorepos-intro-12.3/my-turborepo/packages/ui/src/card.tsx
@@ -2,12 +2,10 @@ import { type JSX } from "react";
 
 export function Card({
   className,
-  title,
   children,
   href,
 }: {
   className?: string;
-  title: string;
   children: React.ReactNode;
   href: string;
 }): JSX.Element {
